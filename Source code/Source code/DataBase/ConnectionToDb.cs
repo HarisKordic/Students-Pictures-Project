f93aa790d31f3bdeaf14c9811cfae675d948f1bd
Haris Kordić <kordicharis18@gmail.com>
2022-02-09 13:02:10 +0100

    Created data base class and connections

---
+++ Source code/Source code/DataBase/ConnectionToDb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Source_code.DataBase
+{
+    public  class ConnectionToDb:DbContext
+    {
+        public ConnectionToDb():base("DataBasePath")
+        {
+            
+        }
+    }
+}
