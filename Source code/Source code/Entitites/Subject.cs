79a5e1a72babae8544fe2f0fd6b564098737b057
Haris Kordić <kordicharis18@gmail.com>
2022-02-27 17:03:25 +0100

    Refactored (code clean-up)

--- Source code/Source code/Entitites/Subject.cs
+++ Source code/Source code/Entitites/Subject.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Source_code.Entitites
 {
     [Table("Subjects")]
-    public  class Subject
+    public class Subject
     {
         #region Props
-
         public int Id { get; set; }
         public string Name { get; set; }
         #endregion
