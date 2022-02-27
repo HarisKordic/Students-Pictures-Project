85b223a39a0c4e77885240090ad72d50ee41e096
Haris Kordić <kordicharis18@gmail.com>
2022-02-27 17:01:15 +0100

    Refactored (code clean-up)

--- Source code/Source code/Entitites/StudentPicture.cs
+++ Source code/Source code/Entitites/StudentPicture.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Source_code.Entitites
 {
     [Table("StudentsPictures")]
-    public  class StudentPicture
+    public class StudentPicture
     {
         #region Props
         public int Id { get; set; }
