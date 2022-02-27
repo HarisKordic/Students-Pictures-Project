60e1b7096478e41bbdedcdd9ea5e1029b810f40b
Haris Kordić <kordicharis18@gmail.com>
2022-02-27 17:06:22 +0100

    Refactored (code clean-up)

--- Source code/Source code/Helpers/ImageConverter.cs
+++ Source code/Source code/Helpers/ImageConverter.cs
@@ -6,25 +6,29 @@ namespace Source_code.Helpers
 {
     internal static class ImageConverter
     {
+        #region Image to byte array conversion
         public static byte[] ImageToByte(Image image)
         {
             if (image != null)
             {
-                var stream=new MemoryStream();
-                image.Save(stream,ImageFormat.Jpeg);
+                var stream = new MemoryStream();
+                image.Save(stream, ImageFormat.Jpeg);
                 return stream.ToArray();
             }
             return null;
         }
+        #endregion
 
+        #region Byte array to Image conversion
         public static Image ByteToImage(byte[] stream)
         {
             if (stream != null)
             {
-                var newStream=new MemoryStream(stream);
+                var newStream = new MemoryStream(stream);
                 return Image.FromStream(newStream);
             }
             return null;
         }
+        #endregion
     }
 }
